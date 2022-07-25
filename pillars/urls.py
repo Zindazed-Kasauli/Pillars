98b6a4eaeed0a0e627803086bc954f72d2e60e6b
Zindazed-Kasauli <zindazed2@gmail.com>
2022-07-25 14:12:17 +0300

    project to changed from pillarsStock

--- pillars/urls.py
+++ pillars/urls.py
@@ -1,4 +1,4 @@
-"""pillarsStock URL Configuration
+"""pillars URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
